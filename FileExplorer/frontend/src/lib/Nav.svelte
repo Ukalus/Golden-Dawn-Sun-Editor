fd56d8f8654b97cb73dcba9d59b1cf6a904c086f
ukalus <luhof50@gmail.com>
2024-02-05 00:16:43 +0100

    work on file browser UI

---
+++ FileExplorer/frontend/src/lib/Nav.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { DarkMode, Navbar, NavUl, NavLi} from "flowbite-svelte";
+
+    
+</script>
+
+<Navbar class="p-0 m-0">
+    <NavUl class="p-0 m-0 [&>*]:p-0 [&>*]:m-0">
+        <NavLi>file</NavLi>
+        <NavLi>edit</NavLi>
+        <NavLi>about</NavLi>
+        <NavLi>help</NavLi>
+    </NavUl>
+    
+</Navbar>
--- FileExplorer/frontend/src/routes/+page.svelte
+++ FileExplorer/frontend/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import Greet from "$lib/Greet.svelte";
+    import Nav from "$lib/Nav.svelte";
+	import FileTree from "src/lib/FileTree.svelte";
 </script>
 
-<Greet></Greet>
+<Nav/>
+<FileTree/>
